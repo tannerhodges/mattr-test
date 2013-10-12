f49e7dfdf415a3d49148771b37768e0e06d8a239
Tanner Hodges <austintannerhodges@gmail.com>
2013-10-12 09:29:06 -0400

    Setup HTML skeleton
    Based on the structure of the design, each block of text has been
    wrapped in a semantically appropriate HTML element (e.g, links are
    wrapped in &gt;a&lt; tags, navigations in &gt;nav&lt; and &gt;ul&lt;,
    etc).
    
    Further, those initial elements have been grouped into higher elements
    (e.g, header, section, div, etc).
    
    At this point, the elements ought to be formatted properly with CSS.
    
    Note: in the future, the elements chosen here may be changed due to a
    better understanding of the design or structure, or for other necessary
    improvements.

--- index.php
+++ index.php
@@ -21,78 +21,112 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
+        <!-- Header -->
+        <header>
+            <h1>Mattr</h1>
+            <nav>
+                <ul>
+                    <li><a href="javascript:void(0);">Blog</a></li>
+                    <li><a href="javascript:void(0);">Sign In</a></li>
+                </ul>
+            </nav>
+        </header>
 
-Mattr
+        <!-- Sticky Navigation -->
+        <div class="sticky-nav">
+            <a href="javascript:void(0);">Request an Invitation</a>
+        </div>
 
-Blog
-Sign In
+        <!-- Hero Section -->
+        <section class="hero">
+            <h1>Personalities<br />tell powerful stories.</h1>
+            <p>The key to successful brand engagement lies within the vibrant personalities that make up online audiences. Mattr translates the social data they generate into powerful stories and actionable insights, allowing brands to connect on a deeper level with their targets.</p>
+            <p>
+                Get To know your audience.<br />
+                Request a private beta invitation:
+            </p>
+            <!-- Email Signup -->
+            <form action="">
+                <input type="email" placeholder="your@email.com" />
+                <button type="submit">Submit</button>
+            </form>
+        </section>
 
-Request an Invitation
+        <!-- Main Content -->
+        <div class="main-content">
+            <div class="column one-third">
+                <h2>The App</h2>
+                <p>Mattr allows you to build a more complete view of your social media audience through a comprehensive set of segmentation, comparison, monitoring, and visualization tools.  Explore our Features to learn more.</p>
+            </div>
 
+            <div class="column one-third">
+                <h2>Audience Personality Segments</h2>
+                <p>Decide how you want to look at your audience, based on our Unique Personality Type Analysis and demographics.</p>
+            </div>
 
-Personalities
-tell powerful stories.
+            <div class="column one-third last">
+                <h2>Persona Builder</h2>
+                <p>Combine Personality Traits and Demographic information to create Audience Personas that make sense for your business.</p>
+            </div>
 
-The key to successful brand engagement lies within the vibrant personalities that make up online audiences. Mattr translates the social data they generate into powerful stories and actionable insights, allowing brands to connect on a deeper level with their targets.
+            <div class="column one-third">
+                <h2>Engagement Response by Persona</h2>
+                <p>Which of your social media activities are attacting the audience engagement you’re looking for? See a segmented report on a per-post basis.</p>
+            </div>
 
+            <div class="column one-third">
+                <h2>Social Brand Benchmarking</h2>
+                <p>See how your brand’s audience compares to that of your competition, or your industry. Choose what you want to compare and then view the comparision.</p>
+            </div>
 
-Get To know your audience.
-Request a private beta invitation:
+            <div class="column one-third last">
+                <h2>Sentiment Variance</h2>
+                <p>Knowing that your audience is ‘upbeat’ just isn’t enough. You need to know if they’re more upbeat about your brand than they are about other things.</p>
+            </div>
+        </div>
 
-your@email.com
+        <!-- Footer -->
+        <footer>
+            <div class="column one-fourth">
+                <h3>San Francisco, CA</h3>
+                <ul>
+                    <li>Rocketspace</li>
+                    <li>181 Fremont Street </li>
+                    <li>San Francisco, California 94105</li>
+                </ul>
+                <a href="javascript:void(0);">EMAIL US</a>
+            </div>
+            <div class="column one-fourth">
+                <h3>Austin, TX</h3>
+                <ul>
+                    <li>8140 North Mopac Expressway</li>
+                    <li>Building 1, Suite 150</li>
+                    <li>Austin, Texas 78759</li>
+                </ul>
+                <a href="javascript:void(0);">EMAIL US</a>
+            </div>
+            <div class="column one-fourth">
+                <h3>Elsewhere</h3>
+                <ul>
+                    <li><a href="javascript:void(0);">Twitter</a></li>
+                    <li><a href="javascript:void(0);">Facebook</a></li>
+                    <li><a href="javascript:void(0);">LinkedIn</a></li>
+                </ul>
+            </div>
+            <div class="column one-fourth last">
+                <h3>Company</h3>
+                <ul>
+                    <li><a href="javascript:void(0);">Sign In</a></li>
+                    <li><a href="javascript:void(0);">Blog</a></li>
+                    <li><a href="javascript:void(0);">FAQ</a></li>
+                </ul>
+            </div>
 
-Submit
-
-
-The App
-Mattr allows you to build a more complete view of your social media audience through a comprehensive set of segmentation, comparison, monitoring, and visualization tools.  Explore our Features to learn more.
-
-Audience Personality Segments
-Decide how you want to look at your audience, based on our Unique Personality Type Analysis and demographics.
-
-Persona Builder
-Combine Personality Traits and Demographic information to create Audience Personas that make sense for your business.
-
-Engagement Response by Persona
-Which of your social media activities are attacting the audience engagement you’re looking for? See a segmented report on a per-post basis.
-
-Social Brand Benchmarking
-See how your brand’s audience compares to that of your competition, or your industry. Choose what you want to compare and then view the comparision.
-
-Sentiment Variance
-Knowing that your audience is ‘upbeat’ just isn’t enough. You need to know if they’re more upbeat about your brand than they are about other things.
-
-
-San Francisco, CA
-Rocketspace
-181 Fremont Street 
-San Francisco, California 94105
-
-EMAIL US
-
-
-Austin, TX
-8140 North Mopac Expressway
-Building 1, Suite 150
-Austin, Texas 78759
-
-EMAIL US
-
-
-Elsewhere
-Twitter
-Facebook
-LinkedIn
-
-
-Company
-Sign In
-Blog
-FAQ
-
-
-&copy; 2013 MATTR. All rights reserved.
-        
+            <!-- Copyright -->
+            <div class="copyright">
+                &copy; 2013 MATTR. All rights reserved.
+            </div>
+        </footer>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>

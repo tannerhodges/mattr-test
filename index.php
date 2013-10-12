e8b1cb78c3ac7a6a89428729d395c3e55be6c697
Tanner Hodges <austintannerhodges@gmail.com>
2013-10-12 08:54:32 -0400

    Add copy from reference PSD
    Translating from PSD to HTML requires that the elements from the design
    be reconstructed in code. To begin this process, copy has been copied
    from the PSD and pasted into index.php.
    
    Next, each line of text should be wrapped in the appropriate HTML
    elements.

--- index.php
+++ index.php
@@ -21,6 +21,79 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
+
+Mattr
+
+Blog
+Sign In
+
+Request an Invitation
+
+
+Personalities
+tell powerful stories.
+
+The key to successful brand engagement lies within the vibrant personalities that make up online audiences. Mattr translates the social data they generate into powerful stories and actionable insights, allowing brands to connect on a deeper level with their targets.
+
+
+Get To know your audience.
+Request a private beta invitation:
+
+your@email.com
+
+Submit
+
+
+The App
+Mattr allows you to build a more complete view of your social media audience through a comprehensive set of segmentation, comparison, monitoring, and visualization tools.  Explore our Features to learn more.
+
+Audience Personality Segments
+Decide how you want to look at your audience, based on our Unique Personality Type Analysis and demographics.
+
+Persona Builder
+Combine Personality Traits and Demographic information to create Audience Personas that make sense for your business.
+
+Engagement Response by Persona
+Which of your social media activities are attacting the audience engagement you’re looking for? See a segmented report on a per-post basis.
+
+Social Brand Benchmarking
+See how your brand’s audience compares to that of your competition, or your industry. Choose what you want to compare and then view the comparision.
+
+Sentiment Variance
+Knowing that your audience is ‘upbeat’ just isn’t enough. You need to know if they’re more upbeat about your brand than they are about other things.
+
+
+San Francisco, CA
+Rocketspace
+181 Fremont Street 
+San Francisco, California 94105
+
+EMAIL US
+
+
+Austin, TX
+8140 North Mopac Expressway
+Building 1, Suite 150
+Austin, Texas 78759
+
+EMAIL US
+
+
+Elsewhere
+Twitter
+Facebook
+LinkedIn
+
+
+Company
+Sign In
+Blog
+FAQ
+
+
+&copy; 2013 MATTR. All rights reserved.
+        
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
